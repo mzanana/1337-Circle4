0e72a675f2f4de08e015b39a1b1a7c4aff0d489b
Mohamed Zanana <mr.zananamohamed@gmail.com>
2025-07-01 18:46:51 +0100

    Handle the quotes tokens

--- MiniShell/Project/lexer_main.c
+++ MiniShell/Project/lexer_main.c
@@ -1,13 +1,68 @@
 #include"lexer.h"
 
+
+bool	handle_quoted_token(t_token **tokens, char *input, int *i)
+{
+	char quote_char;
+	int start;
+	int len;
+	char *value;
+	t_token *new_token;
+
+	quote_char = input[*i];
+	(*i)++;
+	start = *i;
+	while (input[*i] && input[*i] != quote_char)
+		(*i)++;
+	if (input[*i] != quote_char)
+	{
+		write(2, "bash: unexpected EOF while looking for matching `", 49);
+		write(2, &quote_char, 1);
+		write(2, "`\n", 2);
+		return (false);
+	}
+	len = (*i) - start;
+	value = ft_strndup(input + start, len);
+	if (!value)
+		return (false);
+	new_token = token_new(value, T_WORD, true, quote_char == '\'');
+	if (!new_token)
+	{
+		free(value);
+		return (false);
+	}
+	token_add_back(tokens, new_token);
+	(*i)++;
+	return (true);
+}
 t_token	*tokenize_input(char *input)
 {
 	t_token *tokens;
+	int		i;
 
+	tokens = NULL;
+	i = 0;
+	while (input[i])
+	{
+		while (input[i] && is_space(input[i]))
+			i++;
+		if (!input[i])
+			break ;
+		if (input[i] == '"' || input[i] == '\'')
+		{
+			if (!handle_quoted_token(&tokens, input, &i))
+			{
+				free_tokens(&tokens);
+				return (NULL);
+			}
+		}
+		i++;
+	}
 	return (tokens);
 }
 
-main()
+
+int main()
 {
 	char *line;
 	t_token *tokens;
