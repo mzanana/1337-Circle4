da7c8e8b1b25d27a666649f53fbaefa62b5645de
mzanana <mr.zananamohamed@gmail.com>
2025-07-29 19:22:59 +0100

    Create the main function of the parser part

--- MiniShell/Parsing/Parser/parser.c
+++ MiniShell/Parsing/Parser/parser.c
@@ -1,15 +1,33 @@
 #include "../parsing_hf.h"
 
-t_cmd token_to_command(t_token *tokens)
-{
-	t_cmd cmdlist;
-	t_token *tmptoken;
 
-	tmptoken = tokens;
-	while (tmptoken)
+t_cmd *token_to_command(t_token *tokens)
+{
+	t_cmd	*ret;
+	t_cmd	*tmp;
+
+	ret = NULL;
+	while (tokens)
 	{
-		
+		tmp = gc_calloc(sizeof(t_cmd));
+		while (tokens && tokens->type != T_PIPE)
+		{
+			if (tokens->type == T_WORD)
+			{
+				cmd_fill(tmp, tokens->value);
+				tokens = tokens->next;
+			}
+			else
+			{
+				red_fill(&(tmp->redir), tokens->type, tokens->next->value, tokens->is_quoted);
+				tokens = tokens->next->next;
+			}
+		}
+		cmd_add_back(&ret, tmp);
+		if (tokens && tokens->type == T_PIPE)
+			tokens = tokens->next;
 	}
-	return cmdlist;
+	return (ret);
 }
+
 
