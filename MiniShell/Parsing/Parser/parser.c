a6a49801e862bf30bc00e0db58230f6c221926cf
mzanana <mr.zananamohamed@gmail.com>
2025-07-30 06:56:40 +0100

    finish the part of adding argv into the command

--- MiniShell/Parsing/Parser/parser.c
+++ MiniShell/Parsing/Parser/parser.c
@@ -1,5 +1,35 @@
 #include "../parsing_hf.h"
 
+int	count_args(t_cmd *cmd)
+{
+	int ret;
+
+	if (!cmd->argv)
+		return (0);
+	ret = 0;
+	while (cmd->argv[ret])
+		ret++;
+	return ret;
+}
+
+void	cmd_argv_fill(t_cmd *cmd, char *value)
+{
+	char	**new_argv;
+	int		length;
+	int		i;
+
+	length = count_args(cmd);
+	new_argv = gc_calloc(sizeof(char*) * (length + 2));
+	if (!new_argv)
+		return ;
+	i = -1;
+	while (cmd->argv[++i])
+		new_argv[i] = cmd->argv[i];
+	new_argv[length] = value;
+	new_argv[length + 1] = NULL;
+	cmd->argv = new_argv;
+}
+
 
 t_cmd *token_to_command(t_token *tokens)
 {
@@ -14,12 +44,12 @@ t_cmd *token_to_command(t_token *tokens)
 		{
 			if (tokens->type == T_WORD)
 			{
-				cmd_fill(tmp, tokens->value);
+				cmd_argv_fill(tmp, tokens->value);
 				tokens = tokens->next;
 			}
 			else
 			{
-				red_fill(&(tmp->redir), tokens->type, tokens->next->value, tokens->is_quoted);
+				cmd_redir_fill(&(tmp->redir), tokens->type, tokens->next->value, tokens->is_quoted);
 				tokens = tokens->next->next;
 			}
 		}
