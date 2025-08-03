2f47b0d4b0976ed8e43040ba86d9a08f44366645
anassamajane <anass.amajane2001@gmail.com>
2025-08-03 18:59:52 +0100

    split the redir function && respect the 25 lines rule

--- MiniShell/execution/redirections.c
+++ MiniShell/execution/redirections.c
@@ -25,57 +25,64 @@ int	handle_heredoc(char *delimiter)
 	return(pipefd[0]);
 }
 
+int	apply_heredoc(t_redir *redir)
+{
+	int	fd;
+
+	fd = handle_heredoc(redir->filename);
+	if (fd == -1)
+	{
+		perror("heredoc");
+		return (-1);
+	}
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+	return (0);
+}
+
+int	open_redirection_file(t_redir *redir)
+{
+	if (redir->type == R_INPUT)
+		return (open(redir->filename, O_RDONLY));
+	else if (redir->type == R_OUTPUT)
+		return (open(redir->filename, O_CREAT | O_WRONLY | O_TRUNC, 0644));
+	else if (redir->type == R_APPAND)
+		return (open(redir->filename, O_CREAT | O_WRONLY | O_APPEND, 0644));
+	return (-1);
+}
+
+int	apply_file_redirection(t_redir *redir, int fd)
+{
+	if (fd == -1)
+	{
+		perror(redir->filename);
+		return (-1);
+	}
+	if (redir->type == R_INPUT)
+		dup2(fd, STDIN_FILENO);
+	else
+		dup2(fd, STDOUT_FILENO);
+	close(fd);
+	return (0);
+}
 int	handle_redirections(t_redir *redir)
 {
 	int	fd;
 
 	while (redir)
 	{
-		if (redir->type == R_HERDOC)//heredoc
+		if (redir->type == R_HERDOC)//R_HEREDOC!
 		{
-			fd = handle_heredoc(redir->filename);
-			if (fd == -1)
-			{
-				perror("heredoc");
+			if (apply_heredoc(redir) == -1)
 				return (-1);
-			}
-			dup2(fd, STDIN_FILENO);
-			close(fd);
 		}
 		else
 		{
-			fd = -1;
-			if (redir->type == R_INPUT)
-				fd = open(redir->filename, O_RDONLY);
-			else if (redir->type == R_OUTPUT)
-				fd = open(redir->filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-			else if (redir->type == R_APPAND)
-				fd = open(redir->filename, O_CREAT | O_WRONLY | O_APPEND, 0644);
-			if (fd == -1)
-			{
-				perror(redir->filename);
+			fd = open_redirection_file(redir);
+			if (apply_file_redirection(redir, fd) == -1)
 				return (-1);
-			}
-			if (redir->type == R_INPUT)
-				dup2(fd, STDIN_FILENO);
-			else
-				dup2(fd, STDOUT_FILENO);
-			close(fd);
 		}
 		redir = redir->next;
 	}
 	return (0);
-}
-
-t_redir	*new_redir(t_redir_type type, char *filename)
-{
-	t_redir	*redir;
-
-	redir = malloc(sizeof(t_redir));
-	if (!redir)
-		return (NULL);
-	redir->type = type;
-	redir->filename = ft_strdup(filename);
-	redir->next = NULL;
-	return (redir);
 }
